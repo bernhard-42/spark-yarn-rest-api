bd3267930f517c6a247666e7940eab448a17a2eb
bernhard <bernhard.walter@me.com>
2016-04-17 19:18:33 +0200

    repartition only for write

--- simple-project/src/main/scala/iris.scala
+++ simple-project/src/main/scala/iris.scala
@@ -7,6 +7,9 @@ import org.apache.spark.sql.SaveMode
 case class Iris(sepalLength: Double, sepalWidth:Double, petalLength:Double, petalWidth:Double, species:String)
 
 object IrisApp {
+
+	val hdfsPrefix = ""
+
  	def main(args: Array[String]) {
 
 		val conf = new SparkConf().setAppName("Iris Application")
@@ -14,14 +17,15 @@ object IrisApp {
 		val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 		import sqlContext.implicits._
 
-		val iris = sc.textFile("/tmp/iris.data")
-		             .filter(row => row.size>0)
+		val iris = sc.textFile(hdfsPrefix + "/tmp/iris.data")
+					 .filter(row => row.size>0)
 		             .map(_.split(","))
 		             .map(row => Iris(row(0).trim.toDouble, row(1).trim.toDouble, row(2).trim.toDouble, row(3).trim.toDouble, row(4)))
 		             .toDF()
-
+		
 		val means = iris.groupBy("species").mean("sepalLength", "sepalWidth", "petalLength", "petalWidth")
-		                .repartition(1)
-		means.write.format("json").mode(SaveMode.Overwrite).save("/tmp/iris/means")
+
+ 		means.repartition(1)
+ 		     .write.format("json").mode(SaveMode.Overwrite).save(hdfsPrefix + "/tmp/iris/means")
 	}
 }
